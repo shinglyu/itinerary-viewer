d6c7ee9afd61569f825d881133f43d5a8f2a65cd
Shing Lyu <shing.lyu@gmail.com>
2016-03-24 21:57:46 +0800

    Added select remote file button to toolbar

--- js/toolbar.jsx
+++ js/toolbar.jsx
@@ -2,9 +2,15 @@ var Toolbar = React.createClass({
   enablePlanningMode: function(){
     window.location.href = window.location.href + "&planningMode=1"
   },
+  selectRemoteFile: function(evt){
+    //XXX: hardcoded source_files path; input file can only get filename for security reasons
+    window.location.href = window.location.href.split('?')[0] + "?file=" + "source_files/" + evt.target.value
+  },
   render: function(){
     return (
       <div className="toolbar">
+        <label for="remotefile">Select file</label>
+        <input type='file' id="remotefile" onChange={this.selectRemoteFile} />
         <button onClick={this.enablePlanningMode}>Planning Mode</button>
       </div>
     )
