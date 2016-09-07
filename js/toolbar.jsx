8cbabf2db69333a0e1032e57da1d9a9d67c69d08
Shing Lyu <shing.lyu@gmail.com>
2016-09-07 18:02:51 +0800

    Toggle planning mode

--- js/toolbar.jsx
+++ js/toolbar.jsx
@@ -1,6 +1,17 @@
 var Toolbar = React.createClass({
-  enablePlanningMode: function(){
-    window.location.href = window.location.href + "&planningMode=1"
+  togglePlanningMode: function(){
+    if (window.location.href.indexOf("planningMode=1") > 0){
+      window.location.href = window.location.href.replace("planningMode=1", "");
+    }
+    else {
+      console.log(window.location.href.indexOf("?"))
+      if (window.location.href.indexOf("\?") > 0){
+        window.location.href = window.location.href + "&planningMode=1";
+      }
+      else {
+        window.location.href = window.location.href + "?planningMode=1";
+      }
+    }
   },
   selectRemoteFile: function(evt){
     //XXX: hardcoded source_files path; input file can only get filename for security reasons
@@ -11,7 +22,7 @@ var Toolbar = React.createClass({
       <div className="toolbar">
         <label for="remotefile">Select file</label>
         <input type='file' id="remotefile" onChange={this.selectRemoteFile} />
-        <button onClick={this.enablePlanningMode}>Planning Mode</button>
+        <button onClick={this.togglePlanningMode}>Planning Mode</button>
       </div>
     )
   }
